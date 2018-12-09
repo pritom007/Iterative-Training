5616e9c184b639c1221ce7e0c38470c2aa22fdd7
petrosdebesay <petrosd@student.chalmers.se>
2018-12-09 13:20:42 +0800

    Sigmoid

--- dac_paper-master/dac_paper-master/iteration/data/quadric/algorithm.py
+++ dac_paper-master/dac_paper-master/iteration/data/quadric/algorithm.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+print(sigmoid(1))
+
 #def solve_quad(a, b, c, d, e, iters=1000):
 def solve_quad(a, b, c, iters=100):
     f = lambda x: a * x ** 2 + b * x + c
@@ -12,7 +20,6 @@ def solve_quad(a, b, c, iters=100):
     except:
         return 'x'
 
-import numpy as np
 
 MIN_ROOT = -1
 MAX_ROOT = 1
@@ -41,13 +48,13 @@ print(y[0][0], y[0][1], y[0][2], y[0][3])
 for i in range (100000):
     X, Y = make(1, 4)
     if i < 70000:
-        train_x.write(str(X[0][0])+" "+str(X[0][1])+" "+str(X[0][2])+" "+str(X[0][3])+" "+str(X[0][4]))
-        train_y.write(str(Y[0][0])+" "+str(Y[0][1])+" "+str(Y[0][2])+" "+str(Y[0][3]))
+        train_x.write(str(sigmoid(X[0][0]))+" "+str(sigmoid(X[0][1]))+" "+str(sigmoid(X[0][2]))+" "+str(sigmoid(X[0][3]))+" "+str(sigmoid(X[0][4])))
+        train_y.write(str(sigmoid(Y[0][0])))
         train_x.write("\n")
         train_y.write("\n")
     else:
-        test_x.write(str(X[0][0])+" "+str(X[0][1])+" "+str(X[0][2])+" "+str(X[0][3])+" "+str(X[0][4]))
-        test_y.write(str(Y[0][0])+" "+strY[0][1])+" "+str(Y[0][2])+" "+str(Y[0][3]))
+        test_x.write(str(sigmoid(X[0][0]))+" "+str(sigmoid(X[0][1]))+" "+str(sigmoid(X[0][2]))+" "+str(sigmoid(X[0][3]))+" "+str(sigmoid(X[0][4])))
+        test_y.write(str(sigmoid(Y[0][0])))
         test_x.write("\n")
         test_y.write("\n")
 
